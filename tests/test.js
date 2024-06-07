b5de1b9d78e75af8b6d0623b1a1c02c93d8317f7
nex_e6 <nex_e6@outlook.com>
2024-06-07 14:06:16 +1000

    Give up on new algorithm

--- tests/test.js
+++ tests/test.js
@@ -2,14 +2,32 @@
 
 function updateModelParameter(config, controlKeyIsDown, requestData) {
 
-    if (requestData.model === '4o') {
-        if (config.quotaSaving.harshFiltering && !controlKeyIsDown) {
+    // if (requestData.model == 'auto') {
+    //     if (controlKeyIsDown) {
+    //         return requestData;
+    //     }
+    //     requestData.model = config.quotaSaving.model;
+    //     return requestData
+    // }
+    // if (!controlKeyIsDown && config.quotaSaving.harshFiltering) {
+    //     requestData.model = config.quotaSaving.model;
+    // }
+
+
+    // if (requestData.model == 'auto' && !controlKeyIsDown) {
+    //     requestData.model = config.quotaSaving.model;
+    // }
+    // else if (!controlKeyIsDown && config.quotaSaving.harshFiltering) {
+    //     requestData.model = config.quotaSaving.model;
+    // }
+
+    if (!controlKeyIsDown) {
+        if (!requestData.model in config.quotaSaving.exclude) {
             requestData.model = config.quotaSaving.model;
         }
     }
-    else if (!controlKeyIsDown) {
-        requestData.model = config.quotaSaving.model;
-    }
+
+
 
     // requestData.model = prompt("Model", requestData.model);
     // console.log(requestData);
@@ -25,7 +43,8 @@ function updateModelParameter(config, controlKeyIsDown, requestData) {
 function testUpdateModelParameter(model, harshFiltering, controlKeyIsDown, expectedModel) {
     requestData = {model: model}
     config = {quotaSaving: {
-        model: '3.5',
+        model: 'save',
+        exclude: ['4.0'],
         harshFiltering: harshFiltering
     }}
 
@@ -38,22 +57,22 @@ function testUpdateModelParameter(model, harshFiltering, controlKeyIsDown, expec
 }
 
 tests = [
-    ['4o', true, false, '3.5'],
+    ['auto', true, false, 'save'],
+    ['auto', true, true, 'auto'],
+    ['auto', false, false, 'save'],
+    ['auto', false, true, 'auto'],
+    ['4o', true, false, '4o'],
     ['4o', true, true, '4o'],
     ['4o', false, false, '4o'],
     ['4o', false, true, '4o'],
-    ['3.5', true, false, '3.5'],
-    ['3.5', true, true, '3.5'],
-    ['3.5', false, false, '3.5'],
-    ['3.5', false, true, '3.5'],
-    ['auto', true, false, '3.5'],
-    ['auto', true, true, 'auto'],
-    ['auto', false, false, '3.5'],
-    ['auto', false, true, 'auto'],
-    ['4.0', true, false, '3.5'],
+    ['4.0', true, false, 'save'],
     ['4.0', true, true, '4.0'],
     ['4.0', false, false, '4.0'],
-    ['4.0', false, true, '4.0']
+    ['4.0', false, true, '4.0'],
+    ['3.5', true, false, 'save'],
+    ['3.5', true, true, 'save'],
+    ['3.5', false, false, 'save'],
+    ['3.5', false, true, 'save'],
 ]
 
 
